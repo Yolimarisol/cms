26ed7ab480d9ea2842b4c1f91f0b8e5ba7081664
Yolimarisol <yolamarisol@hotmail.com>
2023-06-08 15:37:41 -0600

    Company Model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 
 class Company extends Model
@@ -20,5 +21,19 @@ class Company extends Model
         'address',
         'slug'
     ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function vacancies()
+    {
+        return $this->hasMany(Vacancy::class);
+    }
+
+    public function getExcerptAttribute(): string{
+        return Str::excerpt($this->description);
+    }
 
 }
