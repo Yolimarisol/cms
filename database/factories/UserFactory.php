b5ff41f39aefb6da5d17303adf47b62b7dbc1a3e
Yolimarisol <yolamarisol@hotmail.com>
2023-06-09 09:29:19 -0600

    Factories Ready

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Company;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -18,14 +19,16 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'lastname'=> fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->firstName($gender = null|'male'|'female'),
+            'lastname'=> $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
             'role' => rand(0,2),
-
+            'privacy_policy'=>true,
+            'position_company' => $this->faker->jobTitle(),
+            'companies_id'=> Company::factory()
         ];
     }
 
--- database/factories/VacancyFactory.php
+++ database/factories/VacancyFactory.php
@@ -19,7 +19,17 @@ class VacancyFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->textsentence(),
+            'companies_id'=> Company::factory(),
+            'types_id'=> Type::factory(),
+            'address'=> $this->faker->address(),
+            'requirements'=>'<p>' . implode('</p><p>', $this->faker->paragraphs(2)) . '</p>',
+            'deletion_date'=>now(),
+            'expedition_date' =>now(),
+            'state' => 1,
+            'responsibilities'=>'<p>' . implode('</p><p>', $this->faker->paragraphs(1)) . '</p>',
+            'description'=>'<p>' . implode('</p><p>', $this->faker->paragraphs(2)) . '</p>',
+            'slug' => $this->faker->slug(),
         ];
     }
 }
