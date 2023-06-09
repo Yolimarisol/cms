cabeb4e7e84f71f36741d3c31f56a644956bfff7
Yolimarisol <yolamarisol@hotmail.com>
2023-06-09 09:54:32 -0600

    Seed basic config

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Company;
+use App\Models\Vacancy;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'email' => 'yolandaestrada@elaniin.com',
+            'role'=>0,
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'company',
+            'email' => 'yolamarisol@hotmail.com',
+            'role'=>1,
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'user',
+            'email' => 'yolanda.estrada.ats@gmail.com',
+            'role'=>2,
+        ]);
+
+        $this->call(class: TypeSeeder::class);
+
+        $company = Company::factory(5)->create();
+
+        Vacancy::factory(5)->create([
+            'companies_id' => $company->id,
+        ]);
     }
 }
