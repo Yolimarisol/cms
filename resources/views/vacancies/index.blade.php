9af61591e7aea2ba4a7fb96c6b4fa8df16f59dc9
Yolimarisol <yolamarisol@hotmail.com>
2023-06-14 16:03:58 -0600

    plus style

--- resources/views/vacancies/index.blade.php
+++ resources/views/vacancies/index.blade.php
@@ -1,16 +1,20 @@
 @extends ('layouts.app')
 
 @section('content')
-    <div class= "display mb-3">
-        <h1 class = "mautor">Vacancies</h1>
+    <header class="flex items-center mb-3 py-4">
+        <div class ="flex justify-between w-full items-center">
+            <h1 class = "mautor">Vacancies</h1>
 
-        <a href="/vacancies/create">Create a New Vacancy"</a>
-    </div>
-    <div class = "flex">
+            <a href="/vacancies/create" class="button">Create a New Vacancy"</a>
+        </div>
+    </header>
+    <main class = "flex flex-wrap -mx-3">
         @forelse ( $vacancies as $vacancy)
-            <div class = "bg-white mr-4 p-5 rounded shadow w-1/3 he">
-                <h3 class = "py-6">{{$vacancy->title}}</h3>
-                <div class = "text-grey">{{str_limit($vacancy->description, 150)}}</div>
+            <div class = "w-1/3 px-2.5 pb-6">
+                <div class = "bg-white p-5 rounded shadow he">
+                    <h3 class = "py-6 ">{{$vacancy->title}}</h3>
+                    <div class = "text-grey">{{str_limit($vacancy->description, 150)}}</div>
+                </div>
             </div>
         @empty
             <div>No vacancies yet.</div>
@@ -18,6 +22,6 @@
         @endforelse
 
 
-    </div>
+    </main>
 @endsection
 
--- resources/views/vacancies/show.blade.php
+++ resources/views/vacancies/show.blade.php
@@ -7,6 +7,6 @@
     <div>{{$vacancy->responsibilities}}</div>
     <div>{{$vacancy->requirements}}</div>
     <div>{{$vacancy->address}}</div>
-    <a href="/vacancies">Go back</a>
+    <a href="/vacancies" class="text-grey no-underline" >Go back</a>
 @endsection
 
